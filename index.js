4c2554704964916e578ac10e0eecfa2bf5ff02fb
Cléber Severo <clebersevero1998@gmail.com>
2023-02-27 20:53:26 -0300

    creating an object with the ttitle and the urls that matches the regex and using the text result from the promisse

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import fs from 'fs'; 
 import chalk from 'chalk';
 
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
-
 function extraiLinks(texto) {
-    const regex = /\[([^[\]]*?)\]\(https?:\/\/[^\s?#.].[^\s]*\)/gm; 
-    const capturas = regex.exec(texto);
-    console.log(capturas);
-}
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; 
+    const capturas = [...texto.matchAll(regex)];
+    //const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
 
-extraiLinks(textoTeste);
+    return resultados;
+}
 
 function trataErro(erro) { 
     console.log(erro);
@@ -21,7 +20,8 @@ async function pegaArquivo(caminhoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoArquivo, encoding);
-        console.log(chalk.green(texto));
+        
+        console.log(extraiLinks(texto));;
     } catch (erro) {
         trataErro(erro)
     } finally {
@@ -31,7 +31,7 @@ async function pegaArquivo(caminhoArquivo) {
 
 
 
-//pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/texto.md');
 
 
 // \[[^[\]]*?\]
