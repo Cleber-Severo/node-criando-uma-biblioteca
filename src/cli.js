093be84fdb754593b194249cd4c7942e2b20e29f
Cléber Severo <clebersevero1998@gmail.com>
2023-03-06 19:44:04 -0300

    dealing with errors and giving the file name to the console log

--- src/cli.js
+++ src/cli.js
@@ -4,22 +4,34 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-    console.log(chalk.yellow('lista de Links:'), resultado);
+function imprimeLista(resultado, identificador = '') {
+    console.log(
+        chalk.yellow('lista de Links:'), 
+        chalk.black.bgGreen(identificador),
+        resultado);
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
 
+    try {
+       fs.lstatSync(caminho); 
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.log('arquivo ou diretório não existe');
+            return;
+        }  
+    }
+
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
-        imprimeLista(resultado)
+        imprimeLista(resultado);
 
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async nomeDeArquivo => {
             const lista =  await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-            imprimeLista(lista);
+            imprimeLista(lista, nomeDeArquivo);
         })
     }
 
