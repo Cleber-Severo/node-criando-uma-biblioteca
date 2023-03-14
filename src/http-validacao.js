31b08ca5795d50bf594e62a3daf89d45e9414cc4
Cléber Severo <clebersevero1998@gmail.com>
2023-03-13 21:02:57 -0300

    joining the urls with the status returned from checking the links themselves

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function extraiLinks(arrLinks) {
   return arrLinks.map((objetoLink) => Object.values(objetoLink).join())  
 }
@@ -6,17 +8,33 @@ async function checaStatus(listaURLs) {
 
     const arrStatus = await Promise.all(
         listaURLs.map( async (url) => {
-        const response = await fetch(url);
-        return response.status;
+            try {
+                const response = await fetch(url);
+                return response.status;
+            } catch (error) {
+                return manejaErros(error)
+            }
     }))
 
     return arrStatus
 }
 
+function manejaErros (erro) {
+    if (erro.cause.code === 'ENOTFOUND') {
+        return 'Link não Encontrado';
+    } else {
+        return 'Ocorreu algum erro';
+    }
+}
+
 export default async function lsitaValidada (listaDeLinks) {
     const links = extraiLinks(listaDeLinks);
     const status = await checaStatus(links);
-    return status;
+
+    return listaDeLinks.map((objeto, indice) => ({
+        ...objeto,
+        status: status[indice]
+    }))
+    
 }
 
-//[gatinho salsicha](http://gatinhosalsicha.com.br/)
